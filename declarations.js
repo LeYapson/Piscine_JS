5c53a61ea439c1cf5ec1aee55e11ab9588d2ee18
théau Yapi <theau.yapi@ynov.com>
2023-03-20 15:04:02 +0100

    decrarations.js

---
+++ declarations.js
@@ -0,0 +1,24 @@
+const escapestr = '/\'\"\`\\'
+console.log(escapestr)
+
+const arr =[4, '2']
+console.log(arr)
+
+const obj = {
+    str : "hello",
+
+    num : 42,
+
+    bool : true,
+
+    undef : undefined
+}
+
+const nested = {
+    arr : [3, undefined, '2'],
+    obj : {
+        str : "hello",
+        num : 42,
+        bool : true
+    }
+}
