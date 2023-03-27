c19e35f16e77cac56f24956353897bb035782517
LeYapson <theau.yapi@ynov.com>
2023-03-27 10:54:07 +0200

    mutability.js

--- mutability.js
+++ mutability.js
@@ -2,30 +2,21 @@ const person = {
     name: 'Rick',
     age: 77,
     country: 'US',
-  }
-
-  const clone1 = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
-  const clone2 = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
-  const samePerson = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
+  };
   
+  // Clone1 et Clone2 avec Object.assign()
+  const clone1 = Object.assign({}, person);
+  const clone2 = Object.assign({}, person);
   
-  console.log(person[age] += 1);
-  console.log(person[country] = 'FR');
-  console.log(clone1[clone1])
-  console.log(clone2[clone2])
+  // SamePerson avec la référence à l'objet person original
+  const samePerson = person;
+  
+  // Augmenter l'âge de person de 1 et définir son pays sur 'FR'
+  person.age += 1;
+  person.country = 'FR';
+  
+  console.log(person); // { name: 'Rick', age: 78, country: 'FR' }
+  console.log(clone1); // { name: 'Rick', age: 77, country: 'US' }
+  console.log(clone2); // { name: 'Rick', age: 77, country: 'US' }
+  console.log(samePerson); // { name: 'Rick', age: 78, country: 'FR' }
   
