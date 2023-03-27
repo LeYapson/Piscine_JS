91a916d820ba7922ba3621b4831c3c0d25007fb0
LeYapson <theau.yapi@ynov.com>
2023-03-27 12:19:48 +0200

    last-first-kiss.js

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -4,5 +4,5 @@ console.log(first("wesh"))
 const last = (strR) => strR.slice(-1)
 console.log(last("wesh"))
 
-const kiss = (strK) => arr = [strK[0], strK.slice(-1)]
+const kiss = (strK) => arr == [strK[0], strK.slice(-1)]
 console.log(kiss("wesh"))
