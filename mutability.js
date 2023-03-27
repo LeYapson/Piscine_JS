94ff4face0f13d8b767e248b643ce74ddda9edd9
LeYapson <theau.yapi@ynov.com>
2023-03-27 09:53:02 +0200

    mutability.js

---
+++ mutability.js
@@ -0,0 +1,31 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+
+  const clone1 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+
+  const clone2 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+
+  const samePerson = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+
+  
+  
+  console.log(person[age] += 1);
+  console.log(person[country] = 'FR');
+  console.log(clone1[clone1])
+  console.log(clone2[clone2])
+  
