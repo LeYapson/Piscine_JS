a62974e0ba240ec9b72ba482ace0594175d892ad
LeYapson <theau.yapi@ynov.com>
2023-03-27 11:35:43 +0200

    more-or-less.js

---
+++ more-or-less.js
@@ -0,0 +1,11 @@
+const more = (n) => n + 1
+console.log(more(1))
+
+const less = (n) => n - 1
+console.log(less(2))
+
+const add = (a, b) => a + b
+console.log(add(2, 3))
+
+const sub = (x, y) => x - y
+console.log(sub(3, 2))
