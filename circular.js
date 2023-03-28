b41bea7d4deaf1407227faa7c44b4d058bed53b3
LeYapson <theau.yapi@ynov.com>
2023-03-28 11:32:14 +0200

    circular.js

--- circular.js
+++ circular.js
@@ -1,3 +1,8 @@
-const circular = {
-    circular : console.log(circular(circular))
-}
+let circular = {
+    circular: null
+  };
+  
+  circular.circular = circular; //set circular a circular
+  console.log(circular.circular); // => getter on circular
+  console.log(circular === circular.circular); // => true
+  
