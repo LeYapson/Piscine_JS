7fe41504f648841f9dfed0845ba69420384e7064
LeYapson <theau.yapi@ynov.com>
2023-03-28 10:25:08 +0200

    dog-years.js

--- dog-years.js
+++ dog-years.js
@@ -1,38 +1,37 @@
 function dogYears(planet, ageInSeconds) {
     const earthYearInSeconds = 31557600;
-    let dogAge = ageInSeconds / (earthYearInSeconds * 7);
+    const conversionRate = 7; // 1 human year = 7 dog years
+    let earthAge = ageInSeconds / earthYearInSeconds;
     let orbitalPeriod = 1;
-
-
+  
     switch (planet) {
-        case "mercury":
-            orbitalPeriod = 0.2408467;
-            break;
-        case "venus":
-            orbitalPeriod = 0.61519726;
-            break;
-        case "mars":
-            orbitalPeriod = 1.8808158;
-            break;
-        case "jupiter":
-            orbitalPeriod = 11.862615;
-            break;
-        case "saturne":
-            orbitalPeriod = 29.447498;
-            break;
-        case "uranus":
-            orbitalPeriod = 84.016846;
-            break;
-        case "neptune":
-            orbitalPeriod = 164.79132;
-            break;
-        default:
-            orbitalPeriod = 1;
+      case "mercury":
+        orbitalPeriod = 0.2408467;
+        break;
+      case "venus":
+        orbitalPeriod = 0.61519726;
+        break;
+      case "mars":
+        orbitalPeriod = 1.8808158;
+        break;
+      case "jupiter":
+        orbitalPeriod = 11.862615;
+        break;
+      case "saturn":
+        orbitalPeriod = 29.447498;
+        break;
+      case "uranus":
+        orbitalPeriod = 84.016846;
+        break;
+      case "neptune":
+        orbitalPeriod = 164.79132;
+        break;
+      default:
+        orbitalPeriod = 1;
     }
-
-    let dogAgeOnPlanet = dogAge / orbitalPeriod;
-
+  
+    let dogAgeOnPlanet = (earthAge * conversionRate) / orbitalPeriod;
     dogAgeOnPlanet = Math.round(dogAgeOnPlanet * 100) / 100;
-
-    return dogAgeOnPlanet
-}
+  
+    return dogAgeOnPlanet;
+  }
