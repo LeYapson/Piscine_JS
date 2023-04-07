b7e7a127ef7663e5e17ca28f6706624b1e107c38
LeYapson <theau.yapi@ynov.com>
2023-04-07 11:05:11 +0200

    method-man.js

---
+++ method-man.js
@@ -0,0 +1,19 @@
+const words = (str) => str.split(' ')
+
+console.log(words("un donut sucré au sucre"))
+
+const sentence = (arr) => arr.join(' ')
+
+console.log(sentence(["un","donut","sucré","au","sucre"]))
+
+const yell =(STR) => STR.toUpperCase()
+
+console.log(yell("mais billy, c'est pas homer"))
+
+const whisper = (str) => str.toLowerCase()
+
+console.log(whisper("C'EST HOMERDE!"))
+
+const capitalize = (Str) => Str.charAt(0).toUpperCase() + Str.slice(1);
+
+console.log(capitalize("je suis mort de rire ahah"))
