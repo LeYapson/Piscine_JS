9536fd321a3def1ca36b9571a3089d9523a1cf06
LeYapson <theau.yapi@ynov.com>
2023-04-18 15:43:52 +0200

    min-max.js

---
+++ min-max.js
@@ -0,0 +1,17 @@
+function max(x, y) {
+    if(x>y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(max(3, -56))
+
+function min(x, y) {
+    if(x<y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(min(3, -56))
