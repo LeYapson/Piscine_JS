e6c64e573c20adf1bade895c591d1bf7b5ec69a1
LeYapson <theau.yapi@ynov.com>
2023-05-09 14:40:27 +0200

    js

--- elementary.js
+++ elementary.js
@@ -1,11 +1,29 @@
 function multiply(a, b) {
-    let result = 0;
-    for (let i = 0; i < b; i++) {
-        result += a;
-      }
-      return result
+  // handle cases where one or both inputs are zero
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+
+  // handle cases where one or both inputs are negative
+  let sign = 1;
+  if (a < 0) {
+    sign *= -1;
+    a *= -1;
+  }
+  if (b < 0) {
+    sign *= -1;
+    b *= -1;
+  }
+
+  // perform multiplication using repeated addition
+  let result = 0;
+  for (let i = 0; i < b; i++) {
+    result += a;
+  }
+
+  return sign * result;
 }
-console.log(multiply(6,6))
+console.log(multiply(123,-22))
 
 function divide(a, b, depth = 0) {
     if(a<b) {
