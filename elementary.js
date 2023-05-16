7685ec965c752dfda74e0be4847b9f3b21e0d458
LeYapson <theau.yapi@ynov.com>
2023-05-16 09:27:40 +0200

    elementary.js

--- elementary.js
+++ elementary.js
@@ -1,102 +1,56 @@
 function multiply(a, b) {
-  let negative = false;
-  if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
-    negative = true;
-  }
-
   let result = 0;
-  for (let i = 0; i < myAbs(b); i++) {
-    result += myAbs(a);
+  const sign = (Math.sign(a) === Math.sign(b)) ? 1 : -1;
+  a = Math.abs(a);
+  b = Math.abs(b);
+
+  while (b > 0) {
+    if (b & 1) {
+      result += a;
+    }
+    a <<= 1;
+    b >>= 1;
   }
 
-  if (negative) {
-    result = -result;
-  }
-
-  return result;
+  return sign * result;
 }
 
-function myAbs(num) {
-  if (num < 0) {
-    return -num;
-  }
-  return num;
-}
-console.log(multiply(123,-22))
 
 function divide(a, b) {
-   // If the divisor is 0, return NaN
   if (b === 0) {
-    return NaN;
+    throw new Error("division by zero");
   }
 
-  let quotient = 0;
-  let isNegative = false;
+  const sign = (Math.sign(a) === Math.sign(b)) ? 1 : -1;
+  a = Math.abs(a);
+  b = Math.abs(b);
+  let result = 0;
 
-  // Determine the sign of the quotient
-  if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
-    isNegative = true;
+  while (a >= b) {
+    let shift = 0;
+    while (a >= (b << shift)) {
+      shift += 1;
+    }
+    result += 1 << (shift - 1);
+    a -= b << (shift - 1);
   }
 
-  // Convert inputs to positive numbers
-  a = myAbs2(a);
-  b = myAbs2(b);
+  return sign * result;
+}
 
-  // Perform integer division
+
+function modulo(a, b) {
+  if (b === 0) {
+    throw new Error("division by zero");
+  }
+
+  const sign = (Math.sign(a) === -1) ? -1 : 1;
+  a = Math.abs(a);
+  b = Math.abs(b);
+
   while (a >= b) {
     a -= b;
-    quotient++;
   }
 
-  // Add negative sign if necessary
-  if (isNegative) {
-    quotient = -quotient;
-  }
-
-  return quotient;
+  return sign * a;
 }
-
-function myAbs2(num) {
-  if (num < 0) {
-    return -num;
-  }
-  return num;
-}
-console.log(divide(5,3))
-
-function modulo(divisor, dividend) {
-    // If the divisor is 0, return NaN
-  if (divisor === 0) {
-    return NaN;
-  }
-
-  // If the dividend is 0, return 0
-  if (dividend === 0) {
-    return 0;
-  }
-
-  let isNegative = false;
-
-  // Determine the sign of the remainder
-  if (dividend < 0) {
-    isNegative = true;
-    dividend = -dividend;
-  }
-  if (divisor < 0) {
-    divisor = -divisor;
-  }
-
-  // Perform modulo operation
-  let remainder = dividend;
-  while (remainder >= divisor) {
-    remainder -= divisor;
-  }
-
-  // Add negative sign if necessary
-  if (isNegative) {
-    remainder = -remainder;
-  }
-
-  return remainder;
-}
-  console.log(modulo(34,78))
