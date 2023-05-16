dcfdd2a33d4914e457b6745516403e5ddb350e15
LeYapson <theau.yapi@ynov.com>
2023-05-16 10:47:00 +0200

    slicer.js

--- slicer.js
+++ slicer.js
@@ -1,9 +1,22 @@
 function slice(data, start, end) {
     if (typeof data === 'string') {
+      if (start < 0) {
+        start = data.length + start;
+      }
+      if (end < 0) {
+        end = data.length + end;
+      }
       return data.substring(start, end);
     } else if (Array.isArray(data)) {
+      if (start < 0) {
+        start = data.length + start;
+      }
+      if (end < 0) {
+        end = data.length + end;
+      }
       return data.slice(start, end);
     } else {
       throw new Error('Unsupported data type. Expected string or array.');
     }
   }
+  
