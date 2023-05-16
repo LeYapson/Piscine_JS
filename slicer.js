67cb4a3359999f6dbc188d5317c912d8f3cc5452
LeYapson <theau.yapi@ynov.com>
2023-05-16 10:42:59 +0200

    slicer.js

--- slicer.js
+++ slicer.js
@@ -1,6 +1,6 @@
 function slice(data, start, end) {
     if (typeof data === 'string') {
-      return data.slice(start, end);
+      return data.slice(start, end === undefined ? data.length : end);
     } else if (Array.isArray(data)) {
       return data.slice(start, end);
     } else {
