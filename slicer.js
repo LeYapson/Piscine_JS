7d82a9ae584bfe58c474eff4935bc34e6dfa327b
LeYapson <theau.yapi@ynov.com>
2023-05-16 10:25:40 +0200

    index-of.js

---
+++ slicer.js
@@ -0,0 +1,10 @@
+function slice(data, start, end) {
+    if (typeof data === 'string') {
+      return data.slice(start, end);
+    } else if (Array.isArray(data)) {
+      return data.slice(start, end);
+    } else {
+      throw new Error('Unsupported data type. Expected string or array.');
+    }
+  }
+  
