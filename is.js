fce9e2df317038df13850d9784b03c0ef7f761de
théau Yapi <theau.yapi@ynov.com>
2023-05-29 17:16:40 +0200

    streamdeck upload

--- is.js
+++ is.js
@@ -1,37 +1,25 @@
-//const is = {}
+// Add new function properties to the is object to check value types. Each function should take one argument, and return a boolean.
 
-is.num = function(value) {
-    return typeof value == "number" && !isNaN(value);
-};
-is.nan = function(value) {
-    return typeof value == "number" && isNaN(value);
-};
-is.str = function(value) {
-    return typeof value == "string";
-};
-is.bool= function(value) {
-    return typeof value == "boolean";
-};
-is.undef= function(value) {
-    return typeof value == "undefined";
-};
-is.def= function(value) {
-    return typeof value != "undefined";
-};
-is.arr= function(value) {
-    return Array.isArray(value);
-};
-is.obj= function(value) {
-    return value != null && typeof value === "object";
-};
-is.fun= function(value) {
-    return typeof value == "function";
-};
-is.truthy= function(value) {
-    return Boolean(value);
-};
-is.falsy= function(value) {
-    return !Boolean(value);
-}
+// is.num: value is a number.
+// is.nan: value is NaN.
+// is.str: value is a string.
+// is.bool: value is a boolean.
+// is.undef: value is undefined.
+// is.def: value is defined.
+// is.arr: value is an array.
+// is.obj: value is a simple object or null objects.
+// is.fun: value is a function.
+// is.truthy: value is truthy.
+// is.falsy: value is falsy.
 
-//console.log(is.num(2))
+is.num = (n) => typeof n === 'number'
+is.nan = (n) => Number.isNaN(n)
+is.str = (n) => typeof n === 'string'
+is.bool = (n) => typeof n === 'boolean'
+is.undef = (n) => n === undefined
+is.def = (n) => !is.undef(n)
+is.arr = (n) => Array.isArray(n)
+is.obj = (n) => typeof n === 'object' && !is.fun(n) && !is.arr(n) && n !== null
+is.fun = (n) => typeof n === 'function'
+is.truthy = (n) => !!n
+is.falsy = (n) => !n
