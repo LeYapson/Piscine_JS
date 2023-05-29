9cbf49cc6956efdc6c8cd5dab75b78bca1fcc541
théau Yapi <theau.yapi@ynov.com>
2023-05-29 16:39:09 +0200

    streamdeck upload

--- slicer.js
+++ slicer.js
@@ -1,14 +1,28 @@
-
-function slice(array, begin, end) {
-  let tempArray =[];
-
-  if(end===undefined || end > array.length)
-    end = array.length;
-
-  for (let i = begin; i < end; i++) {
-    tempArray.push(array[i]);
+function slice(str, start = 0, end = str.length) {
+  if (start < 0) {
+      start = -start
+      start = str.length - start
   }
-  return tempArray;
+  if (end < 0) {
+      end = -end
+      end = str.length - end
+  }
+  let res = ''
+  let arr = []
+  for (let i = 0; i <= str.length - 1; i++) {
+      if (i >= start && i < end) {
+          if (typeof str == 'string') {
+              res += str[i]
+          } else if (Array.isArray(str)) {
+              arr.push(str[i])
+          }
+          console.log(str[i])
+      }
+  }
+  if (typeof str == 'string') {
+      return res
+  }
+  return arr
 }
-console.log(slice('abcdef', 2) === 'cdef');
+console.log(slice('abcdef', 0, -2))
 
