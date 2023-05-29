52d24a3bf85d536ea22e9aa3330fb5f288bc5ec5
théau Yapi <theau.yapi@ynov.com>
2023-05-29 16:35:26 +0200

    streamdeck upload

--- slicer.js
+++ slicer.js
@@ -1,11 +1,12 @@
 function slice(data, start, end) {
   if (typeof data === 'string') {
-    return data.slice(start, end);
+    return data.substring(start, end);
   } else if (Array.isArray(data)) {
     return data.slice(start, end);
   } else {
     throw new Error('Unsupported data type. Expected string or array.');
   }
 }
+
 console.log(slice('abcdef', 2) === 'cdef');
 
