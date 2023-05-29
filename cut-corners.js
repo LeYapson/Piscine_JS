c155bf543b112238babe8b3fc1feed8bfabc844a
théau Yapi <theau.yapi@ynov.com>
2023-05-29 16:47:05 +0200

    streamdeck upload

--- cut-corners.js
+++ cut-corners.js
@@ -1,59 +1,72 @@
 function round(n) {
-    let ost
-    let res
-    let flag = false
+    let ost;
+    let res;
+    let flag = false;
+    
     if (n < 0) {
-        n = -n
-        flag = true
+      n = -n;
+      flag = true;
     }
-    ost = n % 1
+  
+    ost = n - parseInt(n);
     if (ost > 0.5) {
-        res = n + 1 - ost
+      res = n + 1 - ost;
     } else {
-        res = n - ost
+      res = n - ost;
     }
+  
     if (flag) {
-        res = -res
+      res = -res;
     }
-    return res
-}
-function ceil(n) {
-    let ost
-    let res
-    ost = n % 1
-    if (n > 0 && ost != 0) {
-        return res = n + 1 - ost
+  
+    return res;
+  }
+  
+  function ceil(n) {
+    let ost;
+    let res;
+    ost = n - parseInt(n);
+    
+    if (n > 0 && ost !== 0) {
+      res = n + 1 - ost;
     } else {
-        return res = n - ost
+      res = n - ost;
     }
-    return n
-}
-function floor(n) {
-    let ost
-    let res
-    ost = n % 1
-    if (n > 0 && ost != 0) {
-        return res = n - ost
+  
+    return res;
+  }
+  
+  function floor(n) {
+    let ost;
+    let res;
+    ost = n - parseInt(n);
+  
+    if (n > 0 && ost !== 0) {
+      res = n - ost;
     } else if (ost === 0) {
-        // console.log(n)
-        return n
+      return n;
     } else {
-        let x
-        x = 1 + ost
-        return res = n - x
+      let x;
+      x = 1 + ost;
+      res = n - x;
     }
-    return n
-}
-function trunc(n) {
-    let ost
-    let res
-    ost = n % 1
-    if (n > 0 && ost != 0) {
-        return res = n - ost
+  
+    return res;
+  }
+  
+  function trunc(n) {
+    let ost;
+    let res;
+    ost = n - parseInt(n);
+  
+    if (n > 0 && ost !== 0) {
+      res = n - ost;
     } else {
-        let x
-        x = 1 + ost
-        return res = n - ost
+      let x;
+      x = 1 + ost;
+      res = n - ost;
     }
-    return n
-}
+  
+    return res;
+  }
+  
