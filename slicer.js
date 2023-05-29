77ad9ea57b202b032e66fafc43cabf23f6008bdd
théau Yapi <theau.yapi@ynov.com>
2023-05-29 16:33:04 +0200

    streamdeck upload

--- slicer.js
+++ slicer.js
@@ -1,21 +1,9 @@
-function slice(input, startIndex, endIndex) {
-  // Convert the input to an array if it is a string
-  if (typeof input === 'string') {
-    input = input.split('');
+function slice(data, start, end) {
+  if (typeof data === 'string') {
+    return data.slice(start, end);
+  } else if (Array.isArray(data)) {
+    return data.slice(start, end);
+  } else {
+    throw new Error('Unsupported data type. Expected string or array.');
   }
-
-  // Calculate the default ending index
-  if (endIndex === undefined) {
-    endIndex = input.length;
-  }
-
-  // Create a new array or string with the sliced elements
-  var result = input.slice(startIndex, endIndex);
-
-  // Convert the result back to a string if the input was a string
-  if (typeof input === 'string') {
-    result = result.join('');
-  }
-
-  return result;
 }
