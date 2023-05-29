5404f33159fb2cdf4eefc81b6fda7ca926209065
théau Yapi <theau.yapi@ynov.com>
2023-05-29 16:38:22 +0200

    streamdeck upload

--- slicer.js
+++ slicer.js
@@ -1,12 +1,14 @@
-function slice(data, start, end) {
-  if (typeof data === 'string') {
-    return data.substring(start, end);
-  } else if (Array.isArray(data)) {
-    return data.slice(start, end);
-  } else {
-    throw new Error('Unsupported data type. Expected string or array.');
-  }
-}
 
+function slice(array, begin, end) {
+  let tempArray =[];
+
+  if(end===undefined || end > array.length)
+    end = array.length;
+
+  for (let i = begin; i < end; i++) {
+    tempArray.push(array[i]);
+  }
+  return tempArray;
+}
 console.log(slice('abcdef', 2) === 'cdef');
 
