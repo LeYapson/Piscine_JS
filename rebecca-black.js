9f6b6fdd8aec5f25838fec59f229824e19f76516
LeYapson <theau.yapi@ynov.com>
2023-06-20 12:56:46 +0200

    rebecca-black.js

--- rebecca-black.js
+++ rebecca-black.js
@@ -6,9 +6,10 @@ function isWeekend(date) {
   return date.getDay() === 6 || date.getDay() === 0;
 }
 
-function isLeapYear(year) {
-    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
-    }
+const isLeapYear = (date) => {
+    let year = date.getFullYear()
+    return new Date(year, 1, 29).getDate() === 29
+}
 
 function lastDayOfMonth(month, year) {
     return new Date(year, month + 1, 0).getDate();
