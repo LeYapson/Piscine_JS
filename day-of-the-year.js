88d64ecfac30b82d878562147d4fea602090006f
LeYapson <theau.yapi@ynov.com>
2023-06-23 09:16:47 +0200

    streamdeck upload

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,24 +1,15 @@
 // Create a function named dayOfTheYear which accepts a Date. Your function should return the number of days since the first day of that year.
 
 function dayOfTheYear(date) {
-    let count = 0
-    let month = date.getMonth()
-    let day = date.getDate()
-    let year = date.getFullYear()
-    let daysInMonth = [31, 28 + countLeapYears(date), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    for (let i = 0; i < month; i++) {
-        count += daysInMonth[i]
-    }
-    count += day
-    return count
-}
-console.log(dayOfTheYear(new Date('0001-01-01')))
-
-function countLeapYears(date) {
-    let count = 0
-    for (let i = 1; i < date.getFullYear(); i++) {
-        if (i % 4 === 0 && i % 100 !== 0) count++
-        else if (i % 400 === 0) count++
-    }
-    return count
-}
+    var start = new Date(date.getFullYear(), 0, 0);
+    var diff = date - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var dayOfYear = Math.floor(diff / oneDay);
+  
+    return dayOfYear;
+  }
+  
+  // Exemple d'utilisation
+  var currentDate = new Date();
+  console.log(dayOfTheYear(currentDate));
+  
